b7bead3116499668a88a91c81eeebc83055e1d11
Bunea Alex <bunealexandru@gmail.com>
2023-07-13 18:31:05 +0300

    Add project files.

---
+++ en-summer-apprentice-net/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace en_summer_apprentice_net
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
