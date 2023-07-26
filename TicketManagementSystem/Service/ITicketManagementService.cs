d56a09cfe16a0a6f70c5bde9a09a764b453a75e6
BuneaAlex <bunealexandru@gmail.com>
2023-07-26 10:13:51 +0300

    Create Service Layer

---
+++ TicketManagementSystem/Service/ITicketManagementService.cs
@@ -0,0 +1,11 @@
+﻿using TicketManagementSystem.Models;
+using TicketManagementSystem.Models.DTOs;
+
+namespace TicketManagementSystem.Service
+{
+    public interface ITicketManagementService
+    {
+        List<Event> GetEvents();
+        List<OrderDTO> GetOrderDTOs();
+    }
+}
---
+++ TicketManagementSystem/Service/TicketManagementService.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using TicketManagementSystem.Models;
+using TicketManagementSystem.Models.DTOs;
+using TicketManagementSystem.Persistence;
+
+namespace TicketManagementSystem.Service
+{
+    public class TicketManagementService :ITicketManagementService
+    {
+        private IEventRepository _eventRepository;
+        private IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+
+        public TicketManagementService(IEventRepository eventRepository,IOrderRepository orderRepository,IMapper mapper) { 
+            _eventRepository = eventRepository;
+            _orderRepository = orderRepository;
+            _mapper = mapper;
+        }
+
+        public List<Event> GetEvents()
+        {
+            return _eventRepository.GetAll().ToList();
+        }
+
+        public List<OrderDTO> GetOrderDTOs()
+        {
+            List<Order> orders = _orderRepository.GetAll().ToList();
+            List<OrderDTO> orderDtos = _mapper.Map<List<OrderDTO>>(orders);
+            return orderDtos;
+        }
+    }
+}
